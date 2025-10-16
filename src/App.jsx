18aba969b3d8be26f593c90a75c3138eea0f15dd
neeta kumari <neetajanvi3@gmail.com>
2025-10-16 12:33:39 +0530

    router

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import News from "./pages/News";
 import { ThemeContext } from "./context/ThemeContext";
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 const App = () => {
   const [articles, setArticles] = useState([]);
@@ -12,7 +12,7 @@ const App = () => {
     document.documentElement.setAttribute("data-theme", theme);
   }, [theme]);
   return (
-    <HashRouter>
+    <BrowserRouter basename="/NewsApp/">
       <Navbar setArticles={setArticles}></Navbar>
       <Routes>
         <Route path="/" element={<News country='us' category='general'    
@@ -58,7 +58,7 @@ const App = () => {
           </Route>
 
        </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 export default App;
