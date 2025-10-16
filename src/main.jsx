9ed3c5de3ae36b9dedb357fa48d41bb7c97f17f7
neeta kumari <neetajanvi3@gmail.com>
2025-10-16 12:22:16 +0530

    path

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,18 @@ import "./index.css";
 import App from "./App.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 import ScrollToTop from "react-scroll-to-top";
+import { BrowserRouter } from "react-router-dom";
 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    
     <ThemeProvider>
+      
       <App />
       <ScrollToTop smooth />
 
     </ThemeProvider>
+    
   </StrictMode>
 );
